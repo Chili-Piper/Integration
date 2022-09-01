3ce57e5da4d2f1fdf2d2da8add9e150828a4d8fe
Chad Allard <104869275+chadallard@users.noreply.github.com>
2022-09-01 16:42:18 -0400

    Complete update to Es6

--- HubSpot/Multiple Checkboxes/submitHandler.js
+++ HubSpot/Multiple Checkboxes/submitHandler.js
@@ -1,35 +1,28 @@
 function handlerSubmit(tenantDomain, routerName, activeForm) {
-  var leadObj = {};
-  var elements = activeForm.elements;
-  // Validate required form input values
-  for (var i = 0, element; (element = elements[i++]); ) {
-    if (element.required && element.value === "") {
-      console.log("form validation failed, some required fields are empty");
-      return;
-    }
-    // Map to lead object
-    switch (element.type) {
-      // Skip buttons
-      case "button" || "submit":
-        continue;
-      // Handle checkboxes
-      case "checkbox":
-        if (element.checked && leadObj[element.name]) {
-          leadObj[element.name] = leadObj[element.name] += "," + element.value;
-        } else if (element.checked) {
-          leadObj[element.name] = element.value;
+    const leadObj = {};
+    const elements = activeForm.elements;
+    // Validate required form input values
+    for (element of elements) {
+        // Map to lead object
+        if (element.required && element.value === "") {
+            return;
         }
-        continue;
-      // All other inputs
-      default:
-        if (element.value) {
-          leadObj[element.name] = element.value;
+        // Handle checkboxes
+        if (element.type == "checkbox") {
+            // Handle checkboxes
+            if (element.checked && leadObj[element.name]) {
+                leadObj[element.name] = leadObj[element.name] += `,${element.value}`;
+            } else if (element.checked) {
+                leadObj[element.name] = element.value;
+            }
+        // Skip buttons
+        } else if (element.type != "button" && element.type == "submit") {
+            if (element.value) {
+                leadObj[element.name] = element.value;
+            }
         }
-        continue;
     }
-  }
-  //Call Chili Piper if validation passed
-  console.log(leadObj);
-  ChiliPiper.submit(tenantDomain, routerName, { map: true, lead: leadObj });
-  return;
+    //Call Chili Piper if validation passed
+    ChiliPiper.submit(tenantDomain, routerName, { map: true, lead: leadObj });
+    return;
 }
